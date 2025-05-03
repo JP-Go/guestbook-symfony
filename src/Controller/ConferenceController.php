84dfea713912e02657e7bcfef47b6decc19283ce
João Pedro <jpedro1580@gmail.com>
2025-05-03 18:58:41 -0300

    Add easter egg for gretting

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'app_conference')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $greet  = '';
+        if($name = $request->query->get('hello')){
+            $greet =  sprintf('<h1>Hello %s</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<EOF
             <html lang="pt-BR">
                 <body>
+                    $greet
                     <img alt="Under construction" src="/images/under-construction.gif" />
                 </body>
             </html>
